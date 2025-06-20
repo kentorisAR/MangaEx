b028dff04ba1edf9ea61f67042b166c7d9d896ee
kentorisAR <kentorisoff@gmail.com>
2025-06-20 22:19:35 +0300

    Update login.js

--- login.js
+++ login.js
@@ -4,13 +4,11 @@
   left: 50%;
   z-index: 9999;
   transform: translate(-50%, -50%);
-  /* Остальные стили для красоты: */
   background: #23233a;
   padding: 32px 24px 24px 24px;
   border-radius: 16px;
   box-shadow: 0 0 38px #00f0ff88, 0 4px 18px #000a;
   min-width: 320px;
   max-width: 90vw;
-  min-height: 0;
   text-align: center;
 }
